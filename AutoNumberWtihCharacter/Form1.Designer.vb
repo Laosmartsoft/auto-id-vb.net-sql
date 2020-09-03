f8bc9be44e8bbb78d7fdc13b04de4de5b2862212
Laosmartsoft <hackerdone@gmail.com>
2020-09-04 00:19:05 +0700

    first commit

---
+++ AutoNumberWtihCharacter/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtSaleID = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dgvSale = New System.Windows.Forms.DataGridView()
+        Me.btnSave = New System.Windows.Forms.Button()
+        CType(Me.dgvSale, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtSaleID
+        '
+        Me.txtSaleID.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSaleID.Location = New System.Drawing.Point(535, 35)
+        Me.txtSaleID.Name = "txtSaleID"
+        Me.txtSaleID.Size = New System.Drawing.Size(177, 35)
+        Me.txtSaleID.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(488, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ID"
+        '
+        'dgvSale
+        '
+        Me.dgvSale.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvSale.Location = New System.Drawing.Point(12, 139)
+        Me.dgvSale.Name = "dgvSale"
+        Me.dgvSale.Size = New System.Drawing.Size(700, 235)
+        Me.dgvSale.TabIndex = 2
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(12, 99)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(92, 34)
+        Me.btnSave.TabIndex = 3
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(727, 386)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.dgvSale)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSaleID)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvSale, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSaleID As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents dgvSale As DataGridView
+    Friend WithEvents btnSave As Button
+End Class
---
+++ AutoNumberWtihCharacter/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim con_str As String = "Data source=HACKER50CC\SQLEXPRESS;Initial catalog=test_db;Integrated Security=true"
+    Dim con As New SqlConnection()
+    Dim cmd As New SqlCommand
+    Dim adapter As New SqlDataAdapter
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        getData()
+        AutoID()
+    End Sub
+
+
+    Sub getData()
+        Using con = New SqlConnection(con_str)
+            Try
+                con.Open()
+                Dim sql As String = "SELECT * FROM tbl_sale"
+                cmd = New SqlCommand(sql, con)
+                adapter = New SqlDataAdapter(cmd)
+                Dim ds As New DataSet
+                adapter.Fill(ds)
+                dgvSale.DataSource = ds.Tables(0)
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                con.Close()
+            End Try
+        End Using
+    End Sub
+
+
+    Sub AutoID()
+        Using con = New SqlConnection(con_str)
+            Try
+                con.Open()
+                Dim sql As String = "SELECT TOP 1 id,sale_id FROM tbl_sale ORDER BY id DESC"
+                cmd = New SqlCommand(sql, con)
+                adapter = New SqlDataAdapter(cmd)
+                Dim ds As New DataSet
+                adapter.Fill(ds)
+
+                If (ds.Tables(0).Rows.Count > 0) Then
+
+                    Dim tmp_id = ds.Tables(0).Rows(0)("sale_id").ToString().Substring(3, 7)
+                    Dim new_id As Integer = CInt(tmp_id) + 1
+                    txtSaleID.Text = "INV" & new_id.ToString("0000000")
+
+                Else
+                    txtSaleID.Text = "INV0000001"
+                End If
+
+
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                con.Close()
+            End Try
+        End Using
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Using con = New SqlConnection(con_str)
+            Try
+                con.Open()
+                Dim sql As String = "INSERT INTO tbl_sale (sale_id,item) VALUES (@sale_id,@item)"
+                cmd = New SqlCommand(sql, con)
+                cmd.Parameters.AddWithValue("sale_id", txtSaleID.Text)
+                cmd.Parameters.AddWithValue("item", "Item " & Now)
+                cmd.ExecuteNonQuery()
+
+
+                getData()
+                AutoID()
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                con.Close()
+            End Try
+        End Using
+    End Sub
+End Class
